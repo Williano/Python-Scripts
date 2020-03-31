29f3e01a8565e2fc9a543c8f256324132cb7be6e
Williano <paawilly17@gmail.com>
2020-03-31 15:07:56 -0500

    Call functions in main to execute program

--- compute_area_of_room/compute_area_of_room.py
+++ compute_area_of_room/compute_area_of_room.py
@@ -22,10 +22,16 @@ def calculate_area_of_room(length:float, width:float):
 
 def display_area_of_room(area:float):
 
-    print(f"The area of the room is {area} feets")
+    print()
+    print(f"The area of the room is {area} meters")
 
 def main():
-    pass
+
+    length, width = get_length_and_width_of_room_from_user()
+
+    area:float = calculate_area_of_room(length, width)
+
+    display_area_of_room(area)
 
 if __name__ == "__main__":
     main()
