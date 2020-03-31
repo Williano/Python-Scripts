7ad8d49608292231ea889e7439b98623c56c8d17
Williano <paawilly17@gmail.com>
2020-03-31 16:07:56 -0500

    Add program description

--- Hello/hello.py
+++ Hello/hello.py
@@ -1,3 +1,12 @@
+"""
+    Script: Hello
+    Description: A python program that asks the user to enter his or her name.
+                 The program then respond with a message that says hello to
+                 the user, using his or her name.
+    Programmer: William Kpabitey Kwabla
+    Date: 03/30/20
+"""
+
 
 def enter_user_name():
 
