c8b2a044a19d07ef91f2e9a3ac30c23ecc89371b
Williano <paawilly17@gmail.com>
2020-03-31 14:55:06 -0500

    Add function to get length and function to compute area

---
+++ compute_area_of_room/compute_area_of_room.py
@@ -0,0 +1,30 @@
+"""
+    Script: Compute Area of a Room
+    Description: A python program that ask user for lenght and width of a room
+                 and compute the area of the room with the user inputs.
+    Programmer: William Kpabitey Kwabla
+    Date: 03/31/20
+"""
+
+
+def get_length_and_width_of_room_from_user():
+
+    length:float = float(input("Enter the length of the room in meters: "))
+    width:float = float(input("Enter the width of the room in meters: "))
+
+    return length, width
+
+def calculate_area_of_room(length:float, width:float):
+
+   area:float = length * width
+
+   return area
+
+def display_area_of_room(area:float):
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
