50d4cc38d869ccb5c870e583bbe2205573a6d8ce
Williano <paawilly17@gmail.com>
2020-04-01 13:12:48 -0500

    Update

--- area_of_a_field/area_of_a_field.py
+++ area_of_a_field/area_of_a_field.py
@@ -11,8 +11,8 @@ ONE_ARCE_CONSTANT:int = 43560
 
 def get_length_and_width_of_field_from_user():
 
-    length:float = float(input("Enter the length of the field in meters: "))
-    width:float = float(input("Enter the width of the field in meters: "))
+    length:float = float(input("Enter the length of the field in feets: "))
+    width:float = float(input("Enter the width of the field in feets: "))
 
     return length, width
 
