13724840df42a7bbecbfd1d32076bc43f7c10739
Williano <paawilly17@gmail.com>
2020-04-03 13:10:53 -0500

    Add function implementations

--- tax_and_tip/tax_and_tip.py
+++ tax_and_tip/tax_and_tip.py
@@ -16,19 +16,34 @@ TAX_CONSTANT: float = 0.17
 
 
 def ask_user_for_cost_of_meal_in_dollars():
-    pass
+
+    cost_of_meal = float(input("Enter price for meal: "))
+
+    return cost_of_meal
 
 def calculate_tax(meal_cost:float):
-    pass
+
+    total_tax = meal_cost * TAX_CONSTANT
+
+    return total_tax
 
 def calculate_tip(meal_cost:float):
-    pass
+
+    total_tip = meal_cost * TIP_CONSTANT
+
+    return total_tip
 
 def calculate_grand_total_for_meal(tip_total:float, tax_total:float):
-    pass
+
+    grand_total_for_meal:float = tip_total + tax_total
+
+    return grand_total_for_meal
 
 def display_grant_total_of_meal(grand_total_of_meal):
-    pass
+
+    print()
+
+    print("The grand total for meal including tax and tip is $: {grand_total_of_meal}")
 
 def main():
     pass
