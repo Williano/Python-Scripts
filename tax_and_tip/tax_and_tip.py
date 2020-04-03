ff2de5117302091f0c4c13ac117f4decfdf31557
Williano <paawilly17@gmail.com>
2020-04-03 12:59:14 -0500

    Add new exercise

---
+++ tax_and_tip/tax_and_tip.py
@@ -0,0 +1,12 @@
+"""
+    Script: Tax and Tip
+    Description: This program ask user for cost of a meal ordered at a
+                 restaurant.
+                 The program then compute the tax and tip for the meal.
+                 The program then displays the tip amount, tax amount,
+                 and the grand total fo the meal including both the
+                 tax and the tip.
+
+    Programmer: William Kpabitey Kwabla.
+    Date: 04/03/2020
+"""
