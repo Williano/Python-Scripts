016c11bdbee8ba5884e143fb8ef604253a81c7b5
Williano <paawilly17@gmail.com>
2020-04-02 21:07:39 -0500

    Add function specifications

---
+++ bottle_deposit/bottle_deposit.py
@@ -0,0 +1,35 @@
+"""
+    Script: Bottle Deposits
+
+    Description: In many jurisdictions a small deposit is added to drink
+                 containers to encourage people to recycle them. In one
+                 particular jurisdiction, drink containers holding one liter or
+                 less have a $0.10 deposit, and drink containers holding more
+                 than one liter have a $0.25 deposit.
+
+                 This program reads the number of containers of each size from
+                 the user.
+                 The program then continue by computing and displaying the
+                 refund that will be received for returning those containers.
+                 It formats the output so that it includes a dollar sign and
+                 always displays exactly two decimal places.
+
+    Programmer: William Kpabitey Kwabla
+    Date: 03/31/20
+"""
+
+
+def get_number_of_containers_of_each_size_from_user():
+    pass
+
+def compute_refund_for_each_container_size():
+    pass
+
+def display_refund_for_each_container_size_in_dollars():
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
