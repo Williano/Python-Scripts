020f8e29fe183a6e08fd9e66339de4d2d88750b2
Williano <paawilly17@gmail.com>
2020-10-22 00:59:15 -0500

    Add melt functionality

--- data_assembly_tidy/data_assembly_tidy.py
+++ data_assembly_tidy/data_assembly_tidy.py
@@ -21,7 +21,19 @@ def main():
     # Load saved file into visits dataframe
     visits_dataframe = pd.read_csv("lhr_visits.csv")
 
-    print(visits_dataframe.head(10))
+    # Melt the data columns into visit length and count
+    visits_melted_dataframe = visits_dataframe.melt(
+        id_vars=["type"],
+        value_vars=[
+        "Up to 12 hrs", "Over 12 hrs to 1 day",
+        "Over 1 day to 2 days" , "Over 2 days to 3 days",
+        "Over 3 days to 4 days", "Over 4 days to 5 days",
+        "Over 5 days to 6 days", "Over 6 days to 1 week",
+        "Over 1 week to 2 weeks", "Over 2 weeks to 3 weeks",
+        "Over 3 weeks to 4 weeks", "Over 4 weeks"],
+        var_name="visit_length", value_name="count")
+
+    print(visits_melted_dataframe.head(100))
     # print(lhr_visit_dom_dataframe.head())
 
     # print("*****************************")
