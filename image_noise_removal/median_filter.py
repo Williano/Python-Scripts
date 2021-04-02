40c1ccea7a50fca1fb49edeceb4bc8209a793da3
Williano <paawilly17@gmail.com>
2021-04-01 22:31:55 -0500

    Add new project

---
+++ image_noise_removal/median_filter.py
@@ -0,0 +1,33 @@
+import cv2
+import numpy as np
+
+
+# Read the image
+img_noisy1 = cv2.imread('sample.png', 0)
+
+# Obtain the number of rows and columns
+# of the image
+m, n = img_noisy1.shape
+
+# Traverse the image. For every 3X3 area,
+# find the median of the pixels and
+# replace the ceter pixel by the median
+img_new1 = np.zeros([m, n])
+
+for i in range(1, m-1):
+    for j in range(1, n-1):
+        temp = [img_noisy1[i-1, j-1],
+               img_noisy1[i-1, j],
+               img_noisy1[i-1, j + 1],
+               img_noisy1[i, j-1],
+               img_noisy1[i, j],
+               img_noisy1[i, j + 1],
+               img_noisy1[i + 1, j-1],
+               img_noisy1[i + 1, j],
+               img_noisy1[i + 1, j + 1]]
+
+        temp = sorted(temp)
+        img_new1[i, j]= temp[4]
+
+img_new1 = img_new1.astype(np.uint8)
+cv2.imwrite('new_median_filtered.png', img_new1)
