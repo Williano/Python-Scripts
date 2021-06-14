7a6c6c9eaae292a7016be5b896b659264aa4d7f2
Williano <paawilly17@gmail.com>
2021-06-14 09:40:37 -0500

    Refactor

--- cps_simulation/cps_simulation.py
+++ cps_simulation/cps_simulation.py
@@ -1,170 +1,115 @@
-"""
-Process communication example
-
-Covers:
-
-- Resources: Store
-
-Scenario:
-  This example shows how to interconnect simulation model elements
-  together using :class:`~simpy.resources.store.Store` for one-to-one,
-  and many-to-one asynchronous processes. For one-to-many a simple
-  BroadCastPipe class is constructed from Store.
-
-When Useful:
-  When a consumer process does not always wait on a generating process
-  and these processes run asynchronously. This example shows how to
-  create a buffer and also tell is the consumer process was late
-  yielding to the event from a generating process.
-
-  This is also useful when some information needs to be broadcast to
-  many receiving processes
-
-  Finally, using pipes can simplify how processes are interconnected to
-  each other in a simulation model.
-
-
-"""
-# Standard library imports
+# Standard Library imports
+import sys
 import random
+import statistics
 
 # Third-party library imports
 import simpy
 
-# Local imports
-from component.component import Component
-from component.fault_injector import FaultInjector
+# Local app imports
+from broadcast_pipe import BroadcastPipe
 
 
 RANDOM_SEED = 42
-SIM_TIME = 100
+SIM_TIME = 200
 
+time_lost: list = []
+data_lost: list = []
+
+
+def message_generator(name, env, out_pipe):
+    """A process which randomly generates messages."""
+    while True:
+        # wait for next transmission
+        yield env.timeout(random.randint(6, 10))
+
+        # messages are time stamped to later check if the consumer was
+        # late getting them.  Note, using event.triggered to do this may
+        # result in failure due to FIFO nature of simulation yields.
+        # (i.e. if at the same env.now, message_generator puts a message
+        # in the pipe first and then message_consumer gets from pipe,
+        # the event.triggered will be True in the other order it will be
+        # False
+        msg = (env.now, '%s says hello at %d' % (name, env.now))
+        out_pipe.put(msg)
+
+
+def message_consumer(name, env, in_pipe):
+    """A process which consumes messages."""
+    while True:
+        # Get event for message pipe
+        msg = yield in_pipe.get()
+
+        if msg[0] < env.now:
+            # if message was already put into pipe, then
+            # message_consumer was late getting to it. Depending on what
+            # is being modeled this, may, or may not have some
+            # significance
+            print('LATE Getting Message: at time %d: %s received message: %s' %
+                  (env.now, name, msg[1]))
+
+            time_lost.append(env.now - msg[0])
+            data_lost.append(sys.getsizeof(msg[1]))
+
+        else:
+            # message_consumer is synchronized with message_generator
+            print('at time %d: %s received message: %s.' %
+                  (env.now, name, msg[1]))
+
+        # Process does some other work, which may result in missing messages
+        yield env.timeout(random.randint(4, 8))
+
+
+def calculate_time_lost(time_lost):
+    average_time_lost = statistics.mean(time_lost)
+
+    # Pretty print the results
+    minutes, frac_minutes = divmod(average_time_lost, 1)
+    seconds = frac_minutes * 60
+
+    return round(minutes), round(seconds)
+
+
+def calculate_data_lost(data_lost):
+    average_data_lost = statistics.mean(data_lost)
+
+    return average_data_lost
 
 
 def main():
+    # Setup and start the simulation
+    print('Process communication')
+    random.seed(RANDOM_SEED)
+    env = simpy.Environment()
 
-  # Setup and start the simulation
-  print('Process communication')
-  random.seed(RANDOM_SEED)
-  env = simpy.Environment()
+    # # For one-to-one or many-to-one type pipes, use Store
+    # pipe = simpy.Store(env)
+    # env.process(message_generator('Generator A', env, pipe))
+    # env.process(message_consumer('Consumer A', env, pipe))
 
-  componentA = Component(env, name="ComponentA", consumers=[], data_received=0, braodcaster="")
-  componentB = Component(env, name="ComponentB", consumers=[componentA], data_received=0, braodcaster="")
-  componentC = Component(env, name="ComponentC", consumers=[], data_received=0, braodcaster="")
-  componentD = Component(env, name="ComponentD", consumers=[], data_received=0, braodcaster="")
-  componentE = Component(env, name="ComponentE", consumers=[], data_received=0, braodcaster="")
+    # print('\nOne-to-one pipe communication\n')
+    # env.run(until=SIM_TIME)
 
-  componentB.send_data_to_consumers()
-  componentA.display_data_from_consumers()
+    # For one-to many use BroadcastPipe
+    # (Note: could also be used for one-to-one,many-to-one or many-to-many)
+    env = simpy.Environment()
+    bc_pipe = BroadcastPipe(env)
 
+    env.process(message_generator('Generator A', env, bc_pipe))
+    env.process(message_consumer('Consumer A', env, bc_pipe.get_output_conn()))
+    env.process(message_consumer('Consumer B', env, bc_pipe.get_output_conn()))
 
-  env.run(until=SIM_TIME)
+    print('\nOne-to-many pipe communication\n')
+
+    env.run(until=SIM_TIME)
+
+    # View results
+    mins, secs = calculate_time_lost(time_lost=time_lost)
+    size_of_data_loss = calculate_data_lost(data_lost=data_lost)
+
+    print(f"Total time lost is {secs} secs")
+
+    print(f"Total size of data lost is {size_of_data_loss} bytes")
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
-
-# class BroadcastPipe(object):
-#     """A Broadcast pipe that allows one process to send messages to many.
-
-#     This construct is useful when message consumers are running at
-#     different rates than message generators and provides an event
-#     buffering to the consuming processes.
-
-#     The parameters are used to create a new
-#     :class:`~simpy.resources.store.Store` instance each time
-#     :meth:`get_output_conn()` is called.
-
-#     """
-#     def __init__(self, env, capacity=simpy.core.Infinity):
-#         self.env = env
-#         self.capacity = capacity
-#         self.pipes = []
-
-#     def put(self, value):
-#         """Broadcast a *value* to all receivers."""
-#         if not self.pipes:
-#             raise RuntimeError('There are no output pipes.')
-#         events = [store.put(value) for store in self.pipes]
-#         return self.env.all_of(events)  # Condition event for all "events"
-
-#     def get_output_conn(self):
-#         """Get a new output connection for this broadcast pipe.
-
-#         The return value is a :class:`~simpy.resources.store.Store`.
-
-#         """
-#         pipe = simpy.Store(self.env, capacity=self.capacity)
-#         self.pipes.append(pipe)
-#         return pipe
-
-
-# def message_generator(name, env, out_pipe):
-#     """A process which randomly generates messages."""
-#     while True:
-#         # wait for next transmission
-#         yield env.timeout(random.randint(6, 10))
-
-#         # messages are time stamped to later check if the consumer was
-#         # late getting them.  Note, using event.triggered to do this may
-#         # result in failure due to FIFO nature of simulation yields.
-#         # (i.e. if at the same env.now, message_generator puts a message
-#         # in the pipe first and then message_consumer gets from pipe,
-#         # the event.triggered will be True in the other order it will be
-#         # False
-#         msg = (env.now, '%s says hello at %d' % (name, env.now))
-#         out_pipe.put(msg)
-
-
-# def message_consumer(name, env, in_pipe):
-#     """A process which consumes messages."""
-#     while True:
-#         # Get event for message pipe
-#         msg = yield in_pipe.get()
-
-#         if msg[0] < env.now:
-#             # if message was already put into pipe, then
-#             # message_consumer was late getting to it. Depending on what
-#             # is being modeled this, may, or may not have some
-#             # significance
-#             print('LATE Getting Message: at time %d: %s received message: %s' %
-#                   (env.now, name, msg[1]))
-
-#         else:
-#             # message_consumer is synchronized with message_generator
-#             print('at time %d: %s received message: %s.' %
-#                   (env.now, name, msg[1]))
-
-#         # Process does some other work, which may result in missing messages
-#         yield env.timeout(random.randint(4, 8))
-
-
-# # Setup and start the simulation
-# print('Process communication')
-# random.seed(RANDOM_SEED)
-# env = simpy.Environment()
-
-# # For one-to-one or many-to-one type pipes, use Store
-# pipe = simpy.Store(env)
-# env.process(message_generator('Generator A', env, pipe))
-# env.process(message_consumer('Consumer A', env, pipe))
-
-# print('\nOne-to-one pipe communication\n')
-# env.run(until=SIM_TIME)
-
-# # For one-to many use BroadcastPipe
-# # (Note: could also be used for one-to-one,many-to-one or many-to-many)
-# env = simpy.Environment()
-# bc_pipe = BroadcastPipe(env)
-
-# env.process(message_generator('Generator A', env, bc_pipe))
-# env.process(message_consumer('Consumer A', env, bc_pipe.get_output_conn()))
-# env.process(message_consumer('Consumer B', env, bc_pipe.get_output_conn()))
-
-# print('\nOne-to-many pipe communication\n')
-# env.run(until=SIM_TIME)
