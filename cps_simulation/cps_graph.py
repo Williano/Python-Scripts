8e4b9d468db4fe9d55834589629fd6e9d490b9d3
Williano <paawilly17@gmail.com>
2021-06-22 17:47:17 -0500

    Update

--- cps_simulation/cps_graph.py
+++ cps_simulation/cps_graph.py
@@ -1,53 +1,56 @@
-from itertools import combinations, groupby
-import random
 import networkx as nx
 import matplotlib.pyplot as plt
 
 
-def gnp_random_connected_graph(nodes, p):
-    """
-    Generates a random undirected graph, similarly to an Erdős-Rényi
-    graph, but enforcing that the resulting graph is conneted
-    """
-    edges = combinations(range(1, nodes), 2)
+def get_cps_data() -> list:
 
-    graph = nx.Graph()
-
-    graph.add_nodes_from(range(1, nodes))
-
-    if p <= 0:
-        return graph
-    if p >= 1:
-        return nx.complete_graph(nodes, create_using=graph)
-
-    for _, node_edges in groupby(edges, key=lambda x: x[0]):
-        node_edges = list(node_edges)
-        random_edge = random.choice(node_edges)
-        graph.add_edge(*random_edge)
-
-
-        for e in node_edges:
-            if random.random() < p:
-                graph.add_edge(*e)
-
-
-    return graph
-
-
-def main():
+    graph_nodes = []
 
     print(" ")
 
     number_of_cps = int(input("How many cyber physical systems do you need in the simulation? (eg. 1, 2, ..): "))
 
-    seed = random.randint(1,10)
-    probability = 0.1
-    G = gnp_random_connected_graph(number_of_cps ,probability)
+    for cps in range(1, number_of_cps + 1):
+
+        print(" ")
+
+        name:str = input(f"Enter the name of {cps}: ")
+        consumer:str = input(f"Enter the name of consumer: ")
+        date_rate = input(f"Enter the data rate(kbps): ")
+
+        cps_info = (name, consumer, {"data_rate": date_rate + "kbps"})
+
+        graph_nodes.append(cps_info)
+
+
+    return graph_nodes
+
+def generate_and_draw_graph(graph_nodes):
+
+    cps_graph = nx.DiGraph()
+
+    cps_graph.add_edges_from(graph_nodes)
+
+    cps_graph.remove_node("0")
 
     plt.figure(figsize=(8,5))
-    nx.draw(G, node_color='lightblue', with_labels=True, node_size=500)
+
+    position = nx.spring_layout(cps_graph)
+
+    nx.draw(cps_graph, position, node_color='lightblue', with_labels=True, node_size=4000)
+
+    rate_label = nx.get_edge_attributes(cps_graph,'data_rate')
+
+    nx.draw_networkx_edge_labels(cps_graph, position, edge_labels=rate_label)
+
     plt.show()
 
+
+def main():
+
+     graph_nodes = get_cps_data()
+
+     generate_and_draw_graph(graph_nodes)
 
 if __name__ == "__main__":
     main()
